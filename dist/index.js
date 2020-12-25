0394bc20439a18b39dc0781782876300feb5c4cf
Evan Wang <evan.wang22@gmail.com>
2020-12-25 12:42:02 -0800

    test

--- dist/index.js
+++ dist/index.js
@@ -81,12 +81,19 @@ const core = __webpack_require__(5127);
 const checkers = __webpack_require__(5397);
 const github = __webpack_require__(3134);
 
+console.log(context.eventName)
+
 const GITHUB_TOKEN = core.getInput("github-token");
 const CLUBHOUSE_TOKEN = core.getInput("clubhouse-token");
 
 async function run() {
   try {
     await checkers.clubhouseAcceptance(github.context.payload.number, GITHUB_TOKEN, CLUBHOUSE_TOKEN);
+
+    // If event is a repo dispatch
+    // 1. find github PR associated w/story
+    // 2. get SHA for latest commit on PR/branch
+    // 3. send post using Clubhouse info on dispatch to github SHA
   } catch (error) {
     console.log(error.stack);
     core.setFailed(error.message);
