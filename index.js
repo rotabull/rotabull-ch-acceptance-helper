ff2f7eadd5e46bd20082bcb17a6b5cfa3cacd435
Evan Wang <evan.wang22@gmail.com>
2020-12-25 14:06:45 -0800

    wip: no longer needed

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const core = require("@actions/core");
 const checkers = require("./lib/checkers");
 const github = require('@actions/github');
 
-console.log(context.eventName)
-
 const GITHUB_TOKEN = core.getInput("github-token");
 const CLUBHOUSE_TOKEN = core.getInput("clubhouse-token");
 
 async function run() {
   try {
-    await checkers.clubhouseAcceptance(github.context.payload.number, GITHUB_TOKEN, CLUBHOUSE_TOKEN);
-
-    // If event is a repo dispatch
-    // 1. find github PR associated w/story
-    // 2. get SHA for latest commit on PR/branch
-    // 3. send post using Clubhouse info on dispatch to github SHA
+    switch (github.context.eventName) {
+      case 'pull_request':
+        await checkers.clubhouseAcceptance(github.context.payload.number, GITHUB_TOKEN, CLUBHOUSE_TOKEN);
+      case 'repository_dispatch':
+      // search for PR based on [chXYZ] in title
+      // get SHA of latest
+      // send post using Clubhouse info on dispatch to github SHA
+      default:
+        console.error("Unsupported Event Type; only `pull_request` and `repository_dispatch` allowed");
+    }
   } catch (error) {
     console.log(error.stack);
     core.setFailed(error.message);
