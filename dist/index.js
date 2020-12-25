e3ff44afea0c997c9b352b320d8877a6665977c0
Evan Wang <evan.wang22@gmail.com>
2020-12-25 14:08:01 -0800

    swap import for require

--- dist/index.js
+++ dist/index.js
@@ -75,41 +75,21 @@ module.exports = JSON.parse("[[[0,44],\"disallowed_STD3_valid\"],[[45,46],\"vali
 /***/ }),
 
 /***/ 5456:
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var fs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5747);
-/* harmony import */ var fs__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(fs__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5127);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_actions_core__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _lib_checkers__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5397);
-/* harmony import */ var _lib_checkers__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_lib_checkers__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(3134);
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_actions_github__WEBPACK_IMPORTED_MODULE_3__);
+const core = __webpack_require__(5127);
+const checkers = __webpack_require__(5397);
+const github = __webpack_require__(3134);
 
-
-
-
-
-const GITHUB_TOKEN = _actions_core__WEBPACK_IMPORTED_MODULE_1___default().getInput("github-token");
-const CLUBHOUSE_TOKEN = _actions_core__WEBPACK_IMPORTED_MODULE_1___default().getInput("clubhouse-token");
-
-const event = JSON.parse(fs__WEBPACK_IMPORTED_MODULE_0___default().readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
+const GITHUB_TOKEN = core.getInput("github-token");
+const CLUBHOUSE_TOKEN = core.getInput("clubhouse-token");
 
 async function run() {
   try {
-    console.log(_actions_github__WEBPACK_IMPORTED_MODULE_3__.context.eventName)
-    await _lib_checkers__WEBPACK_IMPORTED_MODULE_2___default().clubhouseAcceptance(event.number, GITHUB_TOKEN, CLUBHOUSE_TOKEN);
-
-    // If event is a repo dispatch
-    // 1. find github PR associated w/story
-    // 2. get SHA for latest commit on PR/branch
-    // 3. send post using Clubhouse info on dispatch to github SHA
-
+    await checkers.clubhouseAcceptance(github.context.payload.number, GITHUB_TOKEN, CLUBHOUSE_TOKEN);
   } catch (error) {
     console.log(error.stack);
-    _actions_core__WEBPACK_IMPORTED_MODULE_1___default().setFailed(error.message);
+    core.setFailed(error.message);
   }
 }
 
@@ -16791,46 +16771,6 @@ module.exports = require("zlib");;
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => module['default'] :
-/******/ 				() => module;
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/node module decorator */
 /******/ 	(() => {
 /******/ 		__webpack_require__.nmd = (module) => {
--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-import fs from "fs";
-import core from "@actions/core";
-import checkers from "./lib/checkers";
-import { context } from "@actions/github";
+const core = require("@actions/core");
+const checkers = require("./lib/checkers");
+const github = require('@actions/github');
 
 const GITHUB_TOKEN = core.getInput("github-token");
 const CLUBHOUSE_TOKEN = core.getInput("clubhouse-token");
 
-const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
-
 async function run() {
   try {
-    console.log(context.eventName)
-    await checkers.clubhouseAcceptance(event.number, GITHUB_TOKEN, CLUBHOUSE_TOKEN);
-
-    // If event is a repo dispatch
-    // 1. find github PR associated w/story
-    // 2. get SHA for latest commit on PR/branch
-    // 3. send post using Clubhouse info on dispatch to github SHA
-
+    await checkers.clubhouseAcceptance(github.context.payload.number, GITHUB_TOKEN, CLUBHOUSE_TOKEN);
   } catch (error) {
     console.log(error.stack);
     core.setFailed(error.message);
